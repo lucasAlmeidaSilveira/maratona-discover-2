3c84289249b7dae6e0a024336d6e920ec4a92ea0
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2021-04-05 15:53:07 -0300

    Add itens do array jobs no index (id e name)

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const routes = express.Router();
 
-const views = __dirname + '/views/'
+const views = __dirname + "/views/";
 
 const profile = {
   name: "Lucas",
@@ -9,15 +9,30 @@ const profile = {
   "monthly-budget": 3000,
   "days-per-week": 5,
   "hours-per-day": 5,
-  "vacation-per-year": 4
-}
+  "vacation-per-year": 4,
+};
 
-const jobs = []
+const jobs = [
+  {
+    id: 1,
+    name: "Pizzaria Guloso",
+    "daily-hours": 2,
+    "total-hours": 60,
+    createdAt: Date.now(),
+  },
+  {
+    id: 2,
+    name: "OneTwo Projet",
+    "daily-hours": 3,
+    "total-hours": 47,
+    createdAt: Date.now(),
+  },
+
+];
 
 // request, response
-routes.get("/", (req, res) => res.render(views + "index", {profile}));
+routes.get("/", (req, res) => res.render(views + "index", { profile, jobs }));
 routes.get("/job", (req, res) => res.render(views + "job"));
-
 routes.post("/job", (req, res) => {
   // req.body = { name: 'dawd', 'daily-hours': '3', 'total-hours': '32' }
   // const ladtId = jobs[jobs.length - 1]?.id || 1  => algoritmo da aula
@@ -25,15 +40,16 @@ routes.post("/job", (req, res) => {
   jobs.push({
     id: jobs.length + 1,
     name: req.body.name,
-    'daily-hours': req.body["daily-hours"],
-    'total-hours': req.body["total-hours"],
-    createdAt: Date.now() // atribuindo data de hoje
-  })
-  console.log(jobs)
-  return res.redirect('/')
+    "daily-hours": req.body["daily-hours"],
+    "total-hours": req.body["total-hours"],
+    createdAt: Date.now(), // atribuindo data de hoje
+  });
+  console.log(jobs);
+  return res.redirect("/");
 });
-
 routes.get("/job/edit", (req, res) => res.render(views + "job-edit"));
-routes.get("/profile", (req, res) => res.render(views + "profile", {profile}));
+routes.get("/profile", (req, res) =>
+  res.render(views + "profile", { profile }),
+);
 
 module.exports = routes;
