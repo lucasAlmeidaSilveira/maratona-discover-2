3380a48e5f97316465411ea543754578c13cf002
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2021-04-05 15:11:06 -0300

    Atribuindo novos jobs com method Post

--- src/routes.js
+++ src/routes.js
@@ -12,9 +12,27 @@ const profile = {
   "vacation-per-year": 4
 }
 
+const jobs = []
+
 // request, response
 routes.get("/", (req, res) => res.render(views + "index", {profile}));
 routes.get("/job", (req, res) => res.render(views + "job"));
+
+routes.post("/job", (req, res) => {
+  // req.body = { name: 'dawd', 'daily-hours': '3', 'total-hours': '32' }
+  // const ladtId = jobs[jobs.length - 1]?.id || 1  => algoritmo da aula
+
+  jobs.push({
+    id: jobs.length + 1,
+    name: req.body.name,
+    'daily-hours': req.body["daily-hours"],
+    'total-hours': req.body["total-hours"],
+    createdAt: Date.now() // atribuindo data de hoje
+  })
+  console.log(jobs)
+  return res.redirect('/')
+});
+
 routes.get("/job/edit", (req, res) => res.render(views + "job-edit"));
 routes.get("/profile", (req, res) => res.render(views + "profile", {profile}));
 
