927d852fe7516eb7440bd92f1d222ea3f285d311
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2021-04-04 23:46:54 -0300

    changes

--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,7 @@ const profile = {
 }
 
 // request, response
-routes.get("/", (req, res) => res.render(views + "index"));
+routes.get("/", (req, res) => res.render(views + "index", {profile}));
 routes.get("/job", (req, res) => res.render(views + "job"));
 routes.get("/job/edit", (req, res) => res.render(views + "job-edit"));
 routes.get("/profile", (req, res) => res.render(views + "profile", {profile}));
