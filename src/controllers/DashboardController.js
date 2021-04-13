fa33e7a59cefc4f7111ee92f6d4a494ac4843de3
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2021-04-13 00:25:40 -0300

    Reset db

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -3,40 +3,48 @@ const JobUtils = require("../utils/JobUtils");
 const Profile = require("../model/Profile");
 
 module.exports = {
-  async index(req, res) {
-    const jobs = await Job.get();
-    const profile = await Profile.get();
+    async index(req, res) {
+        const jobs = await Job.get();
+        const profile = await Profile.get();
 
-    let statusCount = {
-      progress: 0,
-      done: 0,
-      total: jobs.length
-    }
+        let statusCount = {
+            progress: 0,
+            done: 0,
+            total: jobs.length,
+        };
 
-    // total de horas por dia de cada job em progress
-    let jobTotalHours = 0
+        // total de horas por dia de cada job em progress
+        let jobTotalHours = 0;
 
-    const updatedJobs = jobs.map((job) => {
-      // ajustes no job
-      const remaining = JobUtils.remainingDays(job);
-      const status = remaining <= 0 ? "done" : "progress";
+        const updatedJobs = jobs.map((job) => {
+            // ajustes no job
+            const remaining = JobUtils.remainingDays(job);
+            const status = remaining <= 0 ? "done" : "progress";
 
-      // total de horas por dia de cada Job em progress
-      jobTotalHours = status == 'progress' ? jobTotalHours + Number(job['daily-hours']) : jobTotalHours
+            // total de horas por dia de cada Job em progress
+            jobTotalHours =
+                status == "progress"
+                    ? jobTotalHours + Number(job["daily-hours"])
+                    : jobTotalHours;
 
-      // Somando a quantidade de status
-      statusCount[status] += 1
+            // Somando a quantidade de status
+            statusCount[status] += 1;
 
-      return {
-        ...job,
-        remaining,
-        status,
-        budget: JobUtils.calculateBudget(job, profile["value-hour"]),
-      };
-    });
+            return {
+                ...job,
+                remaining,
+                status,
+                budget: JobUtils.calculateBudget(job, profile["value-hour"]),
+            };
+        });
 
-    // qtd de horas que quero trabalhar dia (PROFILE) MENOS qtd de horas/dia de cada Job em progress
-    const freeHours = profile['hours-per-day'] - jobTotalHours
-    return res.render("index", { jobs: updatedJobs, profile, statusCount, freeHours });
-  },
+        // qtd de horas que quero trabalhar dia (PROFILE) MENOS qtd de horas/dia de cada Job em progress
+        const freeHours = profile["hours-per-day"] - jobTotalHours;
+        return res.render("index", {
+            jobs: updatedJobs,
+            profile,
+            statusCount,
+            freeHours,
+        });
+    },
 };
--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -3,61 +3,61 @@ const JobUtils = require("../utils/JobUtils");
 const Profile = require("../model/Profile");
 
 module.exports = {
-  create(req, res) {
-    return res.render("job");
-  },
+    create(req, res) {
+        return res.render("job");
+    },
 
-  async save(req, res) {
-    // const jobs = await Job.get();
-    // req.body = { name: 'dawd', 'daily-hours': '3', 'total-hours': '32' }
-    // const lastId = jobs[jobs.length - 1]?.id || 0; // algoritmo da aula
+    async save(req, res) {
+        // const jobs = await Job.get();
+        // req.body = { name: 'dawd', 'daily-hours': '3', 'total-hours': '32' }
+        // const lastId = jobs[jobs.length - 1]?.id || 0; // algoritmo da aula
 
-    await Job.create({
-      // id: lastId + 1,
-      name: req.body.name,
-      "daily-hours": req.body["daily-hours"],
-      "total-hours": req.body["total-hours"],
-      createdAt: Date.now(), // atribuindo data de hoje
-    })
-    
-    return res.redirect("/");
-  },
+        await Job.create({
+            // id: lastId + 1,
+            name: req.body.name,
+            "daily-hours": req.body["daily-hours"],
+            "total-hours": req.body["total-hours"],
+            createdAt: Date.now(), // atribuindo data de hoje
+        });
 
-  async show(req, res) {
-    const jobs = await Job.get();
-    const jobId = req.params.id;
+        return res.redirect("/");
+    },
 
-    const job = jobs.find((job) => job.id == jobId);
+    async show(req, res) {
+        const jobs = await Job.get();
+        const jobId = req.params.id;
 
-    if (!job) {
-      return res.send("Job not found");
-    }
+        const job = jobs.find((job) => job.id == jobId);
 
-    const profile = await Profile.get();
+        if (!job) {
+            return res.send("Job not found");
+        }
 
-    job.budget = JobUtils.calculateBudget(job, profile["value-hour"]);
+        const profile = await Profile.get();
 
-    return res.render("job-edit", { job });
-  },
+        job.budget = JobUtils.calculateBudget(job, profile["value-hour"]);
 
-  async update(req, res) {
-    const jobId = req.params.id;
-    
-    const updatedJob = {
-      name: req.body.name,
-      "total-hours": req.body["total-hours"],
-      "daily-hours": req.body["daily-hours"],
-    };
+        return res.render("job-edit", { job });
+    },
 
-    await Job.update(updatedJob, jobId);
+    async update(req, res) {
+        const jobId = req.params.id;
 
-    res.redirect("/job/" + jobId);
-  },
+        const updatedJob = {
+            name: req.body.name,
+            "total-hours": req.body["total-hours"],
+            "daily-hours": req.body["daily-hours"],
+        };
 
-  async delete(req, res) {
-    const jobId = req.params.id;
-    await Job.delete(jobId);
+        await Job.update(updatedJob, jobId);
 
-    res.redirect("/");
-  },
+        res.redirect("/job/" + jobId);
+    },
+
+    async delete(req, res) {
+        const jobId = req.params.id;
+        await Job.delete(jobId);
+
+        res.redirect("/");
+    },
 };
